b9dcdb64b0479cef65504a1370690a402cf57884
Darsh-Bhoraniya <darshpatel269@gmail.comgit config --global user.name Darsh-Bhoraniyagit config --global user.email darshpatel269@gmail.com>
2024-07-02 09:17:11 +0530

    env file added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import necessary modules from React and ReactDOM
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import './tailwind.css';
 import App from './App'; // Ensure App component is imported correctly
